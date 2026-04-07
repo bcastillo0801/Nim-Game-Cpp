a4dc38353b57ecfec76ac92c8570051fd3310070
bcastillo0801 <brayancastillo0801@umes.edu.gt>
2026-04-06 23:21:02 -0600

    codigo fuente en c++

---
+++ nim_game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Función Principal
+int main() {
+
+    // Declarar variables 
+    printf("--------WELCOME TO NIM GAME--------");
+
+    // Ingresos de nombres de jugadores
+
+    // Iniciar ciclo do while
+
+    // Menú Principal
+
+    // Opciones
+
+    return 0;
+} 
