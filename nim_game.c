c016fa27db459cc2eb4d5b47bbc7132238befb03
cox randy <coxrandymes.edu.gt>
2026-04-08 00:10:18 -0600

    declaracion de variavles y ciclo while

--- nim_game.c
+++ nim_game.c
@@ -3,8 +3,15 @@
 #include <time.h>
 int main() {
 
+   string jugador1, jugador2;
+int puntos1 = 0, puntos2 = 0, totalPartidas = 0;
+int opcion; // Variable para almacenar la opción del usuario
+bool continuarPrograma = true; // Variable booleana para mantener el ciclo while
+   
+   
     printf("--------WELCOME TO NIM GAME--------");
 
+    
 
     return 0;
 } 
