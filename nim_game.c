982e23b433cccfd60b69bc76fc44a619e622db8c
cox randy <coxrandymes.edu.gt>
2026-04-08 00:36:17 -0600

    arreglo de errores sobre variables y estructura while para el menu

--- nim_game.c
+++ nim_game.c
@@ -1,41 +1,45 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <time.h>
+
+using namespace std;
+
 int main() {
-
-   string jugador1, jugador2;
-int puntos1 = 0, puntos2 = 0, totalPartidas = 0;
-int opcion; // Variable para almacenar la opción del usuario
-bool continuarPrograma = true; // Variable booleana para mantener el ciclo while
-   
-   
-    printf("--------WELCOME TO NIM GAME--------");
-
-    cout << "Nombre del Jugador 1: ";cin >> jugador1;
-cout << "Nombre del Jugador 2: ";cin >> jugador2;
-
-
-do {
-        // Menú Principal 
-        cout << "\n--- MENU PRINCIPAL ---" << endl;
-        cout << "1. Iniciar Juego" << endl;
-        cout << "2. Instrucciones" << endl;
-        cout << "3. Salir" << endl;
-        cout << "Seleccione una opcion: ";
-        cin >> opcion;
-
-} while (continuarPrograma);
-
-( adentro del do{ )
-	if (opcion == 1) {
-	    //Iniciar Juego
-	} else if (opcion == 2) {
-            //MostrarInstrucciones
-        } else if (opcion == 3) {
-            continuarPrograma = false;
-        } 
-
-
-
-    return 0;
-} 
+	
+	string jugador1, jugador2;
+	int puntos1 = 0, puntos2 = 0, totalPartidas = 0;
+	int opcion;
+	bool continuarPrograma = true;
+	
+	cout << "------- WELCOME TO NIM_GAME -------" << endl;
+	cout << "Nombre del Jugador 1: "; cin >> jugador1;
+	cout << "Nombre del Jugador 2: "; cin >> jugador2;
+	
+	do {// menú Principal
+		cout << "\n--- MENU PRINCIPAL ---" << endl;
+		cout << "1. Iniciar Juego" << endl;
+		cout << "2. Instrucciones" << endl;
+		cout << "3. Salir" << endl;
+		cout << "Seleccione una opcion: ";
+		cin >> opcion;
+	
+		if (opcion == 1) {
+			cout <<"\nIniciando el juego..."<< endl;
+		} 
+		else if (opcion == 2) {
+			//mostrar Instrucciones
+			cout << "\nInstrucciones: Quita piezas hasta que el otro pierda." << endl;
+		} 
+		else if (opcion == 3) {
+			continuarPrograma = false;
+			cout <<"Saliendo del programa..."<< endl;
+		}
+		else {
+			cout <<"Opcion no valida, intenta de nuevo."<< endl;
+		}
+		
+	} while (continuarPrograma);
+	
+	return 0;
+}
