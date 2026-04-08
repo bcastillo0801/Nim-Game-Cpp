e213b3600772cbd4d394d04fd1f392f136d929ca
cox randy <coxrandymes.edu.gt>
2026-04-08 01:05:09 -0600

    hicimos un menu para el usuario y se agrego opciones al menu pata que el usuario elija que hacer

--- nim_game.cpp
+++ nim_game.cpp
@@ -19,12 +19,34 @@ int main() {
     cout << "------- WELCOME TO NIM_GAME -------" << endl;
 	cout << "Nombre del Jugador 1: "; cin >> jugador1;
 	cout << "Nombre del Jugador 2: "; cin >> jugador2;
-
-    // Iniciar ciclo do while
-
-    // Menú Principal
-
+  
+    // Menú Principal e inicio del ciclo do while
+   
+    do {
+		cout << "\n--- MENU PRINCIPAL ---" << endl;
+		cout << "1. Iniciar Juego" << endl;
+		cout << "2. Instrucciones" << endl;
+		cout << "3. Salir" << endl;
+		cout << "Seleccione una opcion: ";
+		cin >> opcion;
     // Opciones
+if (opcion == 1) {
+			cout <<"\nIniciando el juego..."<< endl;
+		} 
+		else if (opcion == 2) {
+			//mostrar Instrucciones
+			cout << "\nInstrucciones: Quita piezas hasta que el otro pierda." << endl;
+		} 
+		else if (opcion == 3) {
+			continuarPrograma = false;
+			cout <<"Saliendo del programa..."<< endl;
+		}
+		else {
+			cout <<"Opcion no valida, intenta de nuevo."<< endl;
+		}
+		
+	} while (continuarPrograma);
+
 
     return 0;
 } 
