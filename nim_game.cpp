0b60859febba9f9f47045fdecc9d6ea51b8ed35f
castilloBrayan <ordonezcastillo20@gmail.com>
2026-04-08 19:05:19 -0600

    cambio de nomenclatura camel a snake case

--- nim_game.cpp
+++ nim_game.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
 
     // Declarar variables 
-    string jugador1, jugador2;
-	int puntos1 = 0, puntos2 = 0, totalPartidas = 0;
+    string jugador_1, jugador_2;
+	int puntos_1 = 0, puntos_2 = 0, total_partidas = 0;
 	int opcion;
-	bool continuarPrograma = true;
+	bool continuar_programa = true;
     // Ingresos de nombres de jugadores
     cout << "------- WELCOME TO NIM_GAME -------" << endl;
-	cout << "Nombre del Jugador 1: "; cin >> jugador1;
-	cout << "Nombre del Jugador 2: "; cin >> jugador2;
+	cout << "Nombre del Jugador 1: "; cin >> jugador_1;
+	cout << "Nombre del Jugador 2: "; cin >> jugador_2;
   
     // Menú Principal e inicio del ciclo do while
    
@@ -37,14 +37,14 @@ if (opcion == 1) {
 			cout<<"4. virtoria es igual a 3 puntos, empate = a 1 punto, derrota = 0 puntos" <<endl;
 		} 
 		else if (opcion == 3) {
-			continuarPrograma = false;
+			continuar_programa = false;
 			cout <<"Saliendo del programa..."<< endl;
 		}
 		else {
 			cout <<"Opcion no valida, intenta de nuevo."<< endl;
 		}
 		
-	} while (continuarPrograma);
+	} while (continuar_programa);
 
 
     return 0;
